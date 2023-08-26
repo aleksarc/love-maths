01a3be44352821a69b8422beadfa42b0aff4bf9e
Aleksandro Candido <aleksandrocandido@Aleksandros-MacBook-Pro.local>
2023-08-26 13:21:50 +0100

    Add code for the subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -67,6 +69,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -90,7 +94,7 @@ function incrementWrongAnswer() {
 }
 
 /**
- * Displays numbers in the question shown to the user to guess
+ * Displays numbers in the addition question shown to the user to guess
  * @param {*} operand1 
  * @param {*} operand2 
  */
@@ -101,10 +105,17 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
+/**
+ * Displays numbers in the multiplication question shown to the user to guess
+ * @param {*} operand1 
+ * @param {*} operand2 
+ */
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
